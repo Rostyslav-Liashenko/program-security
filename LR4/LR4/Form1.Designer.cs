9657bf0d6342bc3e9d7bf712043ff12800cd4a2a
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2024-04-16 21:35:37 +0300

    feat: get info about user and computer

--- LR4/LR4/Form1.Designer.cs
+++ LR4/LR4/Form1.Designer.cs
@@ -29,11 +29,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(579, 305);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Інформарція про компютер. Варіант №6";
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(294, 265);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(263, 21);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Об\'єм диска, на якому встановлена програма";
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(294, 232);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(263, 21);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Набір дискових пристроїв";
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(294, 199);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(263, 21);
+            this.label15.TabIndex = 13;
+            this.label15.Text = "Ширина екрана";
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(294, 169);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(263, 21);
+            this.label16.TabIndex = 12;
+            this.label16.Text = "Кількість кнопок миші";
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(16, 265);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(263, 21);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Об\'єм диска, на якому встановлена програма";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(16, 232);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(263, 21);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Набір дискових пристроїв";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(16, 199);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(263, 21);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Ширина екрана";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(16, 169);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(263, 21);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Кількість кнопок миші";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(294, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(276, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Шлях до папки із системними файлами";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(294, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 23);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Шлях до папки ОС Windows";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(294, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 23);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Ім\'я компютера";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(294, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Ім\'я користувача";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(16, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Шлях до папки із системними файлами";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(16, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Шлях до папки ОС Windows";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(16, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ім\'я компютера";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ім\'я користувача";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(601, 333);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота №4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+
+        private System.Windows.Forms.Label label11;
+
+        private System.Windows.Forms.Label label10;
+
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.GroupBox groupBox1;
 
         #endregion
     }
--- LR4/LR4/Form1.cs
+++ LR4/LR4/Form1.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace LR4
 {
     public partial class Form1 : Form
     {
+        private string userName;
+        private string computerName;
+        private string pathOs;
+        private string pathOsConfig;
+        private int countButtonMouse;
+        private double widthScreen;
+        private string diskNames;
+        private string selectedDiskVolume;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string getDriveName()
+        {
+            string driverNames = "";
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                driverNames += d.Name + " ";
+            }
+
+            return driverNames;
+        }
+
+        private void initInfoAboutComputer()
+        {
+            userName = Environment.UserName;
+            computerName = Environment.MachineName;
+            pathOs = Environment.GetEnvironmentVariable("SystemRoot");
+            pathOsConfig = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+            countButtonMouse = SystemInformation.MouseButtons;
+            widthScreen = SystemParameters.PrimaryScreenWidth;
+            diskNames = getDriveName();
+        }
+
+        private void showInfoInForm()
+        {
+            label8.Text = userName;
+            label7.Text = computerName;
+            label6.Text = pathOs;
+            label5.Text = pathOsConfig;
+            label16.Text = countButtonMouse.ToString();
+            label15.Text = widthScreen.ToString();
+            label14.Text = diskNames;
+            label13.Text = "Невідомо";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initInfoAboutComputer();
+            showInfoInForm();
         }
     }
 }
