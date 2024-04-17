3dd440f1197315ecb65c9315a20bdc94be5a4252
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2024-04-17 23:32:07 +0300

    feat: add design for LR5

---
+++ LR5/LR5/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LR5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
